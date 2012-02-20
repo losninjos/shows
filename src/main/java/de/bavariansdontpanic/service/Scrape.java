f1c0754a5c44e3488218fe26f15bf54764236a44
Sebastian Sand <bavariansdontpanic@googlemail.com>
2012-02-20 14:52:38 +0100

    shared properties

--- src/main/java/de/bavariansdontpanic/service/Scrape.java
+++ src/main/java/de/bavariansdontpanic/service/Scrape.java
@@ -1,5 +1,29 @@
 package de.bavariansdontpanic.service;
 
-public class Scrape {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Scrape {
+	
+	private static Logger LOG = Logger;
+
+	public void getScrapeProperty(String p) {
+		
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("scrape.conf"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		LOG.debug(prop.getProperty("scrape.site"));
+		
+	}
+	
+	
+	
+	
+
+	
 }
